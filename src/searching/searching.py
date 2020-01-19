f74fedee8a0275a6ee7790a1bbd689cd33636042
Sydney Crumley <srsimps19@gmail.com>
2020-01-19 12:15:55 -0600

    finished the first 2 stretch in searching.py started working on stretch 3 in searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,10 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -15,16 +16,32 @@ def binary_search(arr, target):
   low = 0
   high = len(arr)-1
 
-  # TO-DO: add missing code
-
+  while low <= high:
+    middle = (low+high)//2
+    if target < arr[middle]:
+      high = middle-1
+    elif target > arr[middle]:
+      low = middle+1
+    else:
+      return middle
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
+  low = 0
+  high = len(arr)-1
   middle = (low+high)//2
-
+  
   if len(arr) == 0:
     return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  elif low <= high:
+    if arr[middle] == target:
+      return middle
+    elif arr[middle] > target:
+      return binary_search_recursive(arr, low, middle-1, target)
+    else:
+      return binary_search_recursive(arr, middle+1, high, target)
+  else:
+    return -1
